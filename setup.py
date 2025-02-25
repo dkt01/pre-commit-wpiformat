d84b05b8fa9a0ec57355341f5883894b95854b3b
David K Turner <dkt01@users.noreply.github.com>
2025-02-25 15:02:10 -0600

    Try setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from setuptools import setup
+
+
+setup(
+    name='pre_commit_placeholder_package',
+    version='2025.31',
+    install_requires=['wpiformat==2025.31'],
+)
